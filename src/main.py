676f78871fa95936393a8e1c480424ac69fc99bc
Julio.sales <juliosaels509@gmail.com>
2024-10-07 15:52:58 -0400

    Add de novas telas

--- src/main.py
+++ src/main.py
@@ -1,15 +1,32 @@
 import sys
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
 from ui.MainLogin import Ui_Login
+from ui.MainWindow import Ui_MainWindow
+from ConfigApp import __version__
 
 class AppLogin(QWidget, Ui_Login):
     def __init__(self) -> None:
         super(AppLogin,self).__init__()
         self.setupUi(self)
-        self.usuario = self.text_usuario.text()
-        self.senha = self.text_password.text()
-        
+        self.setWindowTitle(f"Login - My Finance {__version__}")
 
+        self.bnt_entrar.clicked.connect(self.open_system)
+
+
+    def open_system(self):
+        if self.text_password.text() == '123':
+            self.w = MainWindow()
+            self.w.show()
+            self.close()    
+
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self) -> None:
+        super(MainWindow,self).__init__()
+        self.setupUi(self)
+        self.setWindowTitle(f"My Finance {__version__}")
 
 
 if __name__ == "__main__":
